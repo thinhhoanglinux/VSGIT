341698f2d41204dda5e6a3d5e2c5a9c9c0578b44
nguyendinhnhi1996 <dinhnhi3nn1996@gmail.com>
2017-10-04 22:02:14 +0700

    thay doi lan 3

--- VSGIT/Controllers/HomeController.cs
+++ VSGIT/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace VSGIT.Controllers
             return View();
         }
         /// <summary>
-        /// thay doi lan 2
+        /// thay doi lan 3
         /// </summary>
         /// <returns></returns>
         public ActionResult About()
