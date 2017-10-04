b4c1f81d0a89068bab7bea8bb6710dad99c90884
thinhhoanglinux <hoangquocthinh134@gmail.com>
2017-10-04 20:44:31 +0700

    Add project files.

---
+++ VSGIT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VSGIT.MvcApplication" Language="C#" %>
---
+++ VSGIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VSGIT.Startup))]
+namespace VSGIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
